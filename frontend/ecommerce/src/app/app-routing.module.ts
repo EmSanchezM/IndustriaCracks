a5537e64f3b18851a4b25f37d43c4222b0ae0af5
Mario <mariovarela1996@yahoo.com>
2020-07-13 13:08:34 -0600

    firt commit

--- frontend/ecommerce/src/app/app-routing.module.ts
+++ frontend/ecommerce/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ComprasComponent } from './compras/compras.component';
+import{ InicioComponent } from './inicio/inicio.component';
+import {FooterComponent} from './footer/footer.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
 
-  { path: 'compras',  component: ComprasComponent }
+  { path: 'compras',  component: ComprasComponent },
+  { path:'inicio',component:InicioComponent },
+  { path:'footer',component:FooterComponent }
 ];
 
 @NgModule({
--- frontend/ecommerce/src/app/app.module.ts
+++ frontend/ecommerce/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ComprasComponent } from './compras/compras.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ComprasComponent
+    ComprasComponent,
+    InicioComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
