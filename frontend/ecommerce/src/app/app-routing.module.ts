bfa1d43666c8c8b20def34af65fa9d2e026491e6
ElvinMoisesSanchezM <elvinmoises.sanchez@gmail.com>
2020-07-14 18:06:38 -0600

    incompleted register
    registro de usuario

--- frontend/ecommerce/src/app/app-routing.module.ts
+++ frontend/ecommerce/src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
+/*import { NavbarComponent } from './components/shared/navbar/navbar.component';*/
 import { ComprasComponent } from './pages/compras/compras.component';
 
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent },
+  {path: 'registro', component: RegisterComponent},
   {path: 'perfil', component: PerfilComponent},
   {path: 'compras',  component: ComprasComponent }
 
--- frontend/ecommerce/src/app/components/auth/register/register.component.ts
+++ frontend/ecommerce/src/app/components/auth/register/register.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+import {User} from '../../../models/auth/user';
+import {UserService} from '../../../services/auth/user.service';
+import { formatCurrency } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public user: User; 
+  public registerUser: string;
+  public status: string; 
+
+  constructor(private userService:UserService, private router: Router) {
+    this.user = new User(0,'','','','','','','');
+  }
 
   ngOnInit(): void {
   }
 
+  onRegister(registerForm){
+    this.userService.RegisterUser(this.user)
+                    .subscribe(
+                      response => {
+                        console.log(response);
+                        if(response.user){
+                          this.registerUser = response.user;
+                          this.status = 'Registro exitoso';
+                          console.log(this.status);
+                          registerForm.reset();
+                          this.router.navigate(['/login']);
+                        }else{
+                          this.status = 'Fallo el registro';
+                          console.log(this.status);
+
+                        }
+                      },
+                      error =>{console.log(<any>error);}
+                    );
+
+  }
 }
